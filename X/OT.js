33b74c663ba2541063daf6514e998a8d3fc616a9
Esteban D' Fonollosa <141477850+EstebanZxx@users.noreply.github.com>
2023-08-05 14:01:43 +0700

    Add files via upload

---
+++ X/OT.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const readline = require('readline');
+const { exec } = require('child_process');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('𝐏𝐚𝐬𝐭𝐞 𝐄𝐧𝐜𝐫𝐲𝐩𝐭𝐢𝐨𝐧 𝐓𝐞𝐱𝐭 : ', (teks) => {
+  fs.writeFile('XX/MainDecrypt.tnl', teks, (error) => {
+    if (error) {
+      console.error('Terjadi kesalahan saat menyimpan file:', error);
+    } else {
+      console.log('File berhasil disimpan.');
+      runPythonScript();
+    }
+  });
+});
+
+function runPythonScript() {
+  const command = 'python Main.py XX/MainDecrypt.tnl';
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`Stderr: ${stderr}`);
+      return;
+    }
+    console.log(`Stdout: ${stdout}`);
+  });
+  rl.close();
+}
